e2edadebecaf41daeef79bc84ba7baad5d2414b4
Bilal Kazi <bilalahmedkazi@hotmail.com>
2017-08-14 11:28:18 +0530

    Use onload event

--- js/main.js
+++ js/main.js
@@ -1,27 +1,29 @@
-const lazyBg = document.querySelector('.js-lazy-load');
+window.onload = () => {
+    const lazyBg = document.querySelector('.js-lazy-load');
 
-const onTransitionEnd = () => {
-  lazyBg.remove();
-  newBg.classList.remove('fade-in');
-  newBg.style.willChange = '';
-  newBg.removeEventListener('transitionend', onTransitionEnd);
-};
+    const onTransitionEnd = () => {
+        lazyBg.remove();
+        newBg.classList.remove('fade-in');
+        newBg.style.willChange = '';
+        newBg.removeEventListener('transitionend', onTransitionEnd);
+    };
 
-const newBg = document.createElement('div');
-newBg.classList.add('background');
-newBg.classList.add('fade-in');
-newBg.style.willChange = 'opacity';
-newBg.addEventListener('transitionend', onTransitionEnd);
-newBg.style.backgroundImage = `url(${lazyBg.dataset.img})`;
+    const newBg = document.createElement('div');
+    newBg.classList.add('background');
+    newBg.classList.add('fade-in');
+    newBg.style.willChange = 'opacity';
+    newBg.addEventListener('transitionend', onTransitionEnd);
+    newBg.style.backgroundImage = `url(${lazyBg.dataset.img})`;
 
-// Load image to make sure it's in cache
-const img = new Image();
-img.src = lazyBg.dataset.img;
-img.onload = () => {
-    lazyBg.insertAdjacentElement('afterend', newBg);
-    requestAnimationFrame(() => {
+    // Load image to make sure it's in cache
+    const img = new Image();
+    img.src = lazyBg.dataset.img;
+    img.onload = () => {
+        lazyBg.insertAdjacentElement('afterend', newBg);
         requestAnimationFrame(() => {
-            newBg.style.opacity = 1;
+            requestAnimationFrame(() => {
+                newBg.style.opacity = 1;
+            });
         });
-    });
-};
+    };
+}
